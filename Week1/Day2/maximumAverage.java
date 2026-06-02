dd7358bf8a0d3e158bb933c826869af5e3b9aeb1
Aditya Raj <work.adityaraj0513@gmail.com>
2026-06-02 11:05:46 +0530

    Update maximumAverage.java

--- Week1/Day2/maximumAverage.java
+++ Week1/Day2/maximumAverage.java
@@ -1,17 +1,20 @@
 class Solution {
     public double findMaxAverage(int[] nums, int k) {
-        double maxSum = Double.MIN_VALUE;
         double currSum = 0;
-        int i ;
-        for (i = 0; i < k; i++) {
-            currSum = currSum + nums[i];
+        
+        // Step 1: sum of first k elements
+        for (int i = 0; i < k; i++) {
+            currSum += nums[i];
         }
-        int j = i + k - 1;
-        for (i = 1; j < nums.length; i++) {
-            maxSum = Math.max(currSum, maxSum);
-            currSum = currSum + nums[j] - nums[i - 1];
+        
+        double maxSum = currSum;
+        
+        // Step 2: slide the window
+        for (int i = k; i < nums.length; i++) {
+            currSum += nums[i] - nums[i - k];
+            maxSum = Math.max(maxSum, currSum);
         }
-
-        return maxSum/k ;
+        
+        return maxSum / k;
     }
 }
